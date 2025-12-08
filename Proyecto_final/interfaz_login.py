b5c688e426cf50f86907becdf66035189321201a
josephriandevillanueva <joseph.riande.villanueva@gmail.com>
2025-12-07 22:00:54 -0600

    Add files via upload
    avance inicial del proyecto con las primeras versiones de algunas interfaces

---
+++ Proyecto_final/interfaz_login.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from user_streamlit import users
+
+
+def interfaz_login():
+
+    st.title("LOGIN")
+    user = st.text_input("Ingrese su nombre de ususario")
+    password = st.text_input("Ingrese su contraseña", type="password")
+
+
+    if st.button("Iniciar sesion"):
+        u = users()
+        resultado = u.login(user,password)
+        if  resultado:
+         st.success(f"Bienvenido {user} !")
+         st.session_state['usuario'] = resultado
+         st.session_state['logeado'] = True
+
+         if "admin" in user.lower():
+           st.session_state['vista_actual'] = 'menu_admin'
+         else:
+            st.session_state['vista_actual'] = 'menu_medicos'
+         st.rerun()
+         
+         
+         
+         
+       
+
+        else:
+          st.error("Contraseña o Usuario Incorrecto: ")
+
+
+
+        
